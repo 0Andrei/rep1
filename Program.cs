92e2766cef74ecc36f37e89f28a98f131d03c898
marcoweb <marcocarvalho.web@gmail.com>
2023-08-21 14:46:32 +0000

    Estrutura IF

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Condicional
+var flag = 25;
+
+if(flag > 50) {
+    Console.WriteLine("Maior");
+} else {
+    Console.WriteLine("Não é Maior");
+}
+
+switch (flag) {
+    case 50:
+        Console.WriteLine("Valor igual 50");
+    break;
+}
